fd462498580ae7b592b512b6bc7d37666d6ccaf3
MarcelKasulke <marcelkasulke@gmail.com>
2020-03-27 18:15:19 +0100

    test

--- pushoverSend/src/pushoverSend/Initial.java
+++ pushoverSend/src/pushoverSend/Initial.java
@@ -1,5 +1,20 @@
 package pushoverSend;
+import java.io.IOException;
 
-public class Initial {
-
+import net.pushover.client.PushoverException;
+/*###########################################################################################*/
+/*#   Classname                                                                             #*/
+/*###########################################################################################*/
+public class Initial{
+	/* ################################################### */
+	/* # Main-Method                                     # */
+	/* ################################################### */
+	public static void main(String[] args) throws PushoverException, IOException{
+		//Create Instance
+		Notify notify = new Notify();
+		//Show me some data on my phone and transfer it to my Galaxy Watch
+		notify.pushoverNotify("Testnachricht","Handy","Testprotokoll","www.github/xxx","GitHub Account","magic");
+	}
 }
+
+
